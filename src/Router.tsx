0d17af3f87bb7ae257f907a03bae51f4238427a0
JUNU <bokjunwoo@gmail.com>
2023-03-19 22:23:59 +0900

    feat : chakra-ui 설치 및 파일 구조 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Admin from './pages/Admin';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin" />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/*" element={<Navigate to="/admin" replace={true} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
