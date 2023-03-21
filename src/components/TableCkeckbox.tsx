1fcde8758abed1058acc1b9b24feb7ba9b03c47a
JUNU <bokjunwoo@gmail.com>
2023-03-21 15:57:53 +0900

    feat : 주문상태를 관리하는 체크박스 컴포넌트 구현

---
+++ src/components/TableCkeckbox.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { Checkbox, Stack } from '@chakra-ui/react';
+import useSetParams from '@/lib/hooks/useSetParams';
+
+const TableCkeckbox = () => {
+  const { currentDate, onSetParams } = useSetParams();
+  const [isAll, setIsAll] = useState(true);
+  const [isCompletedChecked, setIsCompletedChecked] = useState(false);
+  const [isInCompletedChecked, setIsInCompletedChecked] = useState(false);
+
+  const handleComplete = () => {
+    onSetParams({
+      pageValue: 1,
+      dateValue: `${currentDate || ''}`,
+      statusValue: 'true',
+    });
+    setIsCompletedChecked(!isCompletedChecked);
+    setIsAll(false);
+    setIsInCompletedChecked(false);
+  };
+
+  const handleInComplete = () => {
+    onSetParams({
+      pageValue: 1,
+      dateValue: `${currentDate || ''}`,
+      statusValue: 'false',
+    });
+    setIsInCompletedChecked(!isInCompletedChecked);
+    setIsAll(false);
+    setIsCompletedChecked(false);
+  };
+
+  const handleAll = () => {
+    onSetParams({
+      pageValue: 1,
+      dateValue: `${currentDate || ''}`,
+      statusValue: 'all',
+    });
+    setIsAll(true);
+    setIsCompletedChecked(false);
+    setIsInCompletedChecked(false);
+  };
+
+  return (
+    <Stack
+      direction={['column', 'row']}
+      spacing={[1, 5]}
+      justify="center"
+      p={5}
+    >
+      <Checkbox
+        size="md"
+        colorScheme="green"
+        p={1}
+        borderWidth={2}
+        borderColor="blue.200"
+        borderRadius={5}
+        isChecked={isAll}
+        onChange={handleAll}
+      >
+        모든 주문 상태보기
+      </Checkbox>
+      <Checkbox
+        size="md"
+        colorScheme="green"
+        p={1}
+        borderWidth={2}
+        borderColor="green.200"
+        borderRadius={5}
+        isChecked={isCompletedChecked}
+        onChange={isCompletedChecked ? handleAll : handleComplete}
+      >
+        완료된 주문 상태보기
+      </Checkbox>
+      <Checkbox
+        size="md"
+        colorScheme="green"
+        p={1}
+        borderWidth={2}
+        borderColor="red.200"
+        borderRadius={5}
+        isChecked={isInCompletedChecked}
+        onChange={isInCompletedChecked ? handleAll : handleInComplete}
+      >
+        대기중인 주문 상태보기
+      </Checkbox>
+    </Stack>
+  );
+};
+
+export default TableCkeckbox;
