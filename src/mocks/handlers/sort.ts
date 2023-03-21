91e02b8b60a3114c2f12ebc1c7f940cd32f11f0b
JUNU <bokjunwoo@gmail.com>
2023-03-21 15:56:59 +0900

    fix : ASC 오타수정

--- src/mocks/handlers/sort.ts
+++ src/mocks/handlers/sort.ts
@@ -2,11 +2,11 @@ import { ISortMethods } from '@/interface/mock';
 
 export const sortMethods: ISortMethods = {
   idDESC: (a, b) => b.id - a.id,
-  idACS: (a, b) => a.id - b.id,
+  idASC: (a, b) => a.id - b.id,
   timeDESC: (a, b) =>
     new Date(b.transaction_time).getTime() -
     new Date(a.transaction_time).getTime(),
-  timeACS: (a, b) =>
+  timeASC: (a, b) =>
     new Date(a.transaction_time).getTime() -
     new Date(b.transaction_time).getTime(),
 };
